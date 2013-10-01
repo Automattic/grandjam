946e1e3a64d68623e74bf8b72b50b244fba2ed14
Min Wei <min@automattic.com>
2013-10-01 16:21:06 -0700

    Adding CSS and Footer class
    Added CSS file for styling the returned data, also added a div for
    footer

--- grandjam.php
+++ grandjam.php
@@ -8,6 +8,7 @@
 class Grandjam {
 	function embed_simplenote( $matches, $attr, $url, $rawattr ) {
 		$embed = sprintf( '<div class="embed-simplenote" data-id="%s"></div>', esc_attr( $matches[1] ) );
+		$embed .='<div class="embed-simplenote-footer"><a href='dkajldf'>Original Document</a></div>';
 		return apply_filters( 'embed_simplenote', $embed, $matches, $attr, $url, $rawattr );
 	}
 
@@ -18,7 +19,7 @@ class Grandjam {
 
 	function enqueue_js_css() {
 		wp_enqueue_script( 'grandjam-embed-simplenote', plugins_url( 'embed-simplenote.js', __FILE__ ), array( 'jquery' ), '20130927a', true );
-
+		wp_enqueue_style( 'grandjam-embed-simplenote', plugins_url( 'embed-simplenote.css', __FILE__ ), array(), '20130927a' );
 	}
 }
 add_action( 'init', array( 'Grandjam', 'grandjam_init' ) );
