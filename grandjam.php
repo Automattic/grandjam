40b06ef162cf9f6ef4dc2440bc02673707816cb8
Jeff <alapapa+github@gmail.com>
2013-10-02 14:36:56 -0700

    Staticize the functions to prevent PHP warnings when calling them statically.

--- grandjam.php
+++ grandjam.php
@@ -8,7 +8,7 @@
 class Grandjam {
 	const CACHE_BUSTER = '20131001';
 
-	function embed_simplenote( $matches, $attr, $url, $rawattr ) {
+	static function embed_simplenote( $matches, $attr, $url, $rawattr ) {
 		if ( !isset( $matches[2] ) ) {
 			return $url;
 		}
@@ -22,13 +22,13 @@ class Grandjam {
 		return apply_filters( 'embed_simplenote', $embed, $matches, $attr, $url, $rawattr );
 	}
 
-	function grandjam_init() {
+	static function grandjam_init() {
 		$regex = '#^https?:\/\/(app.simplenote.com|simp.ly)\/publish\/(\S*)#i';
 		wp_embed_register_handler( 'simplenote', $regex, array( 'Grandjam', 'embed_simplenote' ) );
 		self::enqueue_js_css();
 	}
 
-	function enqueue_js_css() {
+	static function enqueue_js_css() {
 		wp_register_script( 'showdown', plugins_url( 'lib/showdown.js', __FILE__ ), array(), self::CACHE_BUSTER, true );
 		wp_enqueue_script( 'grandjam-embed-simplenote', plugins_url( 'embed-simplenote.js', __FILE__ ), array( 'jquery', 'showdown' ), self::CACHE_BUSTER, true );
 		wp_enqueue_style( 'grandjam-embed-simplenote', plugins_url( 'embed-simplenote.css', __FILE__ ), array(), '20130927a' );
